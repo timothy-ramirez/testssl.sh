5e48ae9c142cfe4d44a58d5fa9f9c36f971a4e1e
Your Name <you@example.com>
2018-08-12 16:25:47 -0600

    Update

--- server.py
+++ server.py
@@ -1,9 +1,14 @@
 import os
 from redis import Redis
 from rq import Queue
-
+from worker import handle_job
 q = Queue(connection=Redis(host='redis-15749.c16.us-east-1-3.ec2.cloud.redislabs.com', port=15749, password='FIq4TLlk7wlFvniLQhddCGJQtzDl8Khx'))
 
-def handle_job(data):
-	print('Handling...', data)
 
+
+
+result = q.enqueue(
+		 handle_job, 'http://nvie.com')
+		 
+print('Done!')		 
+			 
---
+++ worker.py
@@ -0,0 +1,3 @@
+
+def handle_job(data):
+	print('Handling...', data)
